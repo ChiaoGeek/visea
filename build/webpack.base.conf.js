6e0570727040e8748589260aed46e6126db70663
ShaneSue <859787524@qq.com>
2017-03-06 15:12:23 +0800

    1. add the vuex store for all,  2. create window beta 0,1  3,cancel the eslint

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,15 +27,15 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: "pre",
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
+      // {                      //不用eslint格式化了,太麻烦
+      //   test: /\.(js|vue)$/,
+      //   loader: 'eslint-loader',
+      //   enforce: "pre",
+      //   include: [resolve('src'), resolve('test')],
+      //   options: {
+      //     formatter: require('eslint-friendly-formatter')
+      //   }
+      // },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by shanesue on 17/3/5.
+ */
+import Vuex from 'vuex'
+import Vue from 'vue'
+import mutation from './mutation'
+import action from './action'
+
+Vue.use(Vuex)
+
+const state = []
+
+
+export default new Vuex.Store({
+  state,
+  mutation,
+  action,
+})
